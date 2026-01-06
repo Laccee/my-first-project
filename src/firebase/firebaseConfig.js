c76bc9ebc8260cfd39f6b7b1c4be6f6c57e7b927
laccee <lacceerose@gmail.com>
2026-01-05 21:44:12 -0500

    firebase hosting setup

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7fWPof352hYqhrAcwZtybXVCFjPaSdZg",
+  authDomain: "daddysoxtail.firebaseapp.com",
+  projectId: "daddysoxtail",
+  storageBucket: "daddysoxtail.firebasestorage.app",
+  messagingSenderId: "435607263376",
+  appId: "1:435607263376:web:76b0fc82bb54c042ffa015"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
